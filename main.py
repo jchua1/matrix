10e63982b559d697c3a66e8406e41f3be5c1622a
jchua1 <jchua@stuy.edu>
2017-02-28 16:15:42 -0500

    almost done

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
+b = [[1, 1, 1, 1],
+     [1, 1, 1, 1],
+     [1, 1, 1, 1],
+     [1, 1, 1, 1]]
 
+a = [[2, 2, 2, 2],
+     [2, 2, 2, 2],
+     [2, 2, 2, 2],
+     [2, 2, 2, 2]]
+
+print_matrix(b)
+matrix_mult(a, b)
+print("--------------------------------------------------------")
+print_matrix(b)
 
 draw_lines( matrix, screen, color )
 display(screen)
--- matrix.py
+++ matrix.py
@@ -2,11 +2,21 @@ import math
 
 
 def print_matrix( matrix ):
-    for i in range(0, len(matrix)):
-        print matrix[i]
+    for r in range(0, len(matrix)):
+        row = ""
+        for c in range(0, len(matrix[r])):
+            row += str(matrix[r][c]) + " "
+        print row + "\n"
     pass
 
 def ident( matrix ):
+    identity = []
+    for i in range(0, len(matrix)):
+        row = [0] * len(matrix)
+        identity.append(row)
+    for i in range(0, len(identity)):
+        identity[i][i] = 1
+    print_matrix(identity)
     pass
 
 def scalar_mult( matrix, s ):
@@ -18,7 +28,17 @@ def scalar_mult( matrix, s ):
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
     temp = []
-    for i in 
+
+    for r in range(0, 4):
+        row = [0] * len(m2[0])
+        for c2 in range(0, len(m2[0])):
+            for c1 in range(0, 4):
+                row[c2] += m1[r][c1] * m2[c1][c2]
+        temp.append(row)
+
+    for tr in range(len(m2)):
+        for tc in range(len(m2[tr])):
+            m2[tr][tc] = temp [tr][tc]
     pass
 
 
